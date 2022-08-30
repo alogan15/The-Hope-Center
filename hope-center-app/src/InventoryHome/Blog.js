650af2ede6f8f1459afcd0aa893c3e79e9f57f7c
jordaneldridge <Jordan@codedifferently.com>
2022-08-30 14:55:23 -0400

    changed the image folder structure

--- hope-center-app/src/InventoryHome/Blog.js
+++ hope-center-app/src/InventoryHome/Blog.js
@@ -23,7 +23,7 @@ const mainFeaturedPost = {
   title: '',
   description:
    "",
-  image: require('/Users/jordaneldridge/dev/Hope Center/The-Hope-Center/hope-center-app/src/HCTitle.png'),
+  image: require('../Images/HCTitle.png'),
   imageText: 'main image description',
   linkText: '',
 };
